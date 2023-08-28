c1f7667eee2f49778d50e6a8d2d0dc6cb4c41e45
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-08-28 21:30:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+//your JS code here. If required
+
+let text = "Do you want to proceed?";
+
+if (confirm(text) === true) {
+    alert("You clicked OK. Proceeding...");
+  } 
+else {
+    alert("You clicked Cancel. Exiting...")
+  }
